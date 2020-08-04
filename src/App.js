027610ceba360d7f545676b8bf47a1dfabe98a99
hientran1399 <50989351+hientran1399@users.noreply.github.com>
2020-08-04 14:06:12 +0700

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BIDesigner-client        
+          BIDesigner-client EC428
         </p>
       </header>
     </div>
